1bbae626d73f1ef23697fd45336ee888b517896e
Richard Reitzfeld <richie.reitzfeld@gmail.com>
2017-10-13 12:58:29 -0400

    Initial commit

---
+++ AnimatingTableView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AnimatingTableView
+//
+//  Created by Richard Reitzfeld on 10/13/17.
+//  Copyright © 2017 Richard Reitzfeld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
